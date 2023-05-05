83abcbeaf6126bdce79285f48472ca5c2e304d10
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-05-05 09:53:56 -0700

    Create question5.cpp

---
+++ assingment1/question5.cpp
@@ -0,0 +1,16 @@
+//swapping of number
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Input two numbere";
+    cin>>a>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"Swapped no "<<a<<" "<<b<<endl;
+    return 0;
+
+
+}
