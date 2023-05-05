3af697067894f8ccc9316db4ad5bfe27599f42df
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-05-05 09:55:52 -0700

    Create question8.cpp

---
+++ assingment1/question8.cpp
@@ -0,0 +1,16 @@
+//ODD and EVEN
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Input a number";
+    cin>>n;
+    if(n%2==0){
+        cout<<"the number is even "<<n<<endl;
+
+    }else{
+        cout<<"The number is odd "<<n<<endl;
+    }
+    return 0;
+}
