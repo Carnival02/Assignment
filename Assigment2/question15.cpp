064376e08a0436de7c05625858389e7e078a3e9d
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-05-13 22:56:59 -0700

    Create question15.cpp

---
+++ Assigment2/question15.cpp
@@ -0,0 +1,17 @@
+//fibbonacii
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n, a=0,b=1,c=0;
+    cout<<"Input the terms ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<c<<endl;
+    a=b;
+
+    b=c;
+    c=a+b;
+    }
+return 0;
+}
