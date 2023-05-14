280cee7f5113730fc7a0bcfe2b64f66d9c5eed23
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2023-05-13 23:08:10 -0700

    Create question21.cpp

---
+++ Assigment2/question21.cpp
@@ -0,0 +1,17 @@
+//sum of elememt of array
+
+#include<iostream>
+using namespace std;
+int main(){
+int arr[10]={1,5,9,4,7,56,44,2,96,69};
+int sum=0;
+for(int i=0;i<=9;i++){
+    sum=sum+arr[i];
+
+}cout<<"sum is "<<sum<<endl;
+return 0;
+
+
+
+
+}
